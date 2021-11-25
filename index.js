bb849a86b9639c31b310978588be984054683e19
adrianC56 <adriancrespog3@gmail.com>
2021-11-24 20:41:32 -0400

    Estructurando Backend

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const express = require('express')
-// import express from 'express'
+const express = require('express');
+const data = require('./assets/data');
 
-const data = require('./assets/data')
 const app = express();
 const port = 3000;
 const URL= 'http://localhost';
 
-app.get('/api/tramites', (req, res) => {
-  res.send(data)
-})
+app.get('/api/tramites/umss', (req, res) => {
+  if(data.length > 0){
+    res.status(200).send(data);
+  }else{
+    res.status(404).send({data: null, message:" data is empty"});
+  }
+});
 
-app.get('/api/tramites/:id', (req, res) => {
+app.get('/api/tramites/umss/:id', (req, res) => {
   const idReq = req.params.id;
   if(idReq< data.length){
-    res.status(200).send({data: data[idReq], id: idReq})
+    res.status(200).send({data: data[idReq]});
   }else{
-    res.status(404).send({data: null, id: idReq, message:" not find data id"})
+    res.status(404).send({data: null, id: idReq, message:" not find data id"});
   }
-})
-
-app.post('/api/tramites', (req, res) => {
-  console.log( req.params.body );
-  res.send('this is a post mrthod!')
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at ${URL}:${port}`)
-})
+  console.log(`Example app listening at ${URL}:${port}`);
+});
